cb96e040f7ac82f868e0e13494f22bde7655ee76
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-05-24 01:53:51 +0600

    install firebase an create authentication

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBVFxMR5xhlHqmM32ixzbITe-h4Nht6J28",
-  authDomain: "robotics-parts-manufacturer.firebaseapp.com",
-  projectId: "robotics-parts-manufacturer",
-  storageBucket: "robotics-parts-manufacturer.appspot.com",
-  messagingSenderId: "239654343707",
-  appId: "1:239654343707:web:cce1fca89cd509ebc814be"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+export default auth;
