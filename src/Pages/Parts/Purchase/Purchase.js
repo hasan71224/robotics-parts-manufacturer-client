991ccdf51eb2a382622d7bfc766cab1c5c1b3018
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-05-24 01:35:43 +0600

    create quantity value fild and change value

--- src/Pages/Parts/Purchase/Purchase.js
+++ src/Pages/Parts/Purchase/Purchase.js
@@ -2,35 +2,49 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Purchase = () => {
-    const {partsId} = useParams();
+    const { partsId } = useParams();
     const [parts, setParts] = useState({});
-    useEffect(()=>{
+    const [purchaseQuantity, setPurchaseQuantity] = useState()
+    useEffect(() => {
         const url = `http://localhost:5000/parts/${partsId}`;
         fetch(url)
-        .then(res=>res.json())
-        .then(data=>setParts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setParts(data))
+    }, [])
+
+    const handleQuantity=()=>{
+        let newQuantity = parseInt(purchaseQuantity)
+        newQuantity = parts.quantity - newQuantity
+        console.log(newQuantity);
+        let partsUpdate= {...parts, quantity: newQuantity}
+        console.log(partsUpdate);
+        setParts(partsUpdate)
+    }
     return (
-        <div className="card lg:max-w-lg bg-base-100 shadow-xl lg:mx-5">
-        <div className="card-body justify-center items-center">
-            <h2 className="text-2xl card-title text-secondary">{parts.name}</h2>
-            <div className="w-32 rounded">
-                <img src={parts.img} alt={parts.name} />
-            </div>
-            <p> <b>Description:</b> {parts.description}</p>
-            <p> <b>Minimum Order:</b> {parts.minimumOrder}</p>
-            <p><b>{parts.quantity} {parts.quantity > 1 ? 'product' : 'product'} Available</b></p>
-            <p><b>Price:</b> $ {parts.price}</p>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl lg:mx-5 ">
+            <div className="card-body ">
+                <div className='card justify-center items-center'>
+                    <h2 className="text-2xl card-title text-secondary mb-3">{parts.name}</h2>
+                    <div className="w-32 rounded">
+                        <img src={parts.img} alt={parts.name} />
+                    </div>
+                </div>
+                <div>
+                    <p> <b>Description:</b> {parts.description}</p>
+                    <p> <b>Minimum Order Products:</b> {parts.minimumOrder}</p>
+                    <p><b>{parts.quantity} {parts.quantity > 1 ? 'product' : 'product'} Available</b></p>
+                    <p><b>Price:</b> $ {parts.price}</p>
+                    <p> <b>Purchase Quantity:</b> <input type="number" onChange={(e)=>setPurchaseQuantity(e.target.value)} placeholder="Purchase Quantity" onBlur={handleQuantity} class="input input-bordered input-sm w-full max-w-xs" /></p>
+                </div>
 
-            <div className="card-actions justify-center">
-                <label
-                    htmlFor="order-modal"
-                    disabled={parts.quantity < parts.minimumOrder}
-                    className="btn btn-sm btn-primary uppercase text-white font-bold bg-gradient-to-r from-primary to-secondary">Confirm Order</label>
+                <div className="card-actions justify-center">
+                    <label
+                        className="btn btn-sm btn-primary uppercase text-white font-bold bg-gradient-to-r from-primary to-secondary">Confirm Order
+                    </label>
 
+                </div>
             </div>
         </div>
-    </div>
     );
 };
 
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVFxMR5xhlHqmM32ixzbITe-h4Nht6J28",
+  authDomain: "robotics-parts-manufacturer.firebaseapp.com",
+  projectId: "robotics-parts-manufacturer",
+  storageBucket: "robotics-parts-manufacturer.appspot.com",
+  messagingSenderId: "239654343707",
+  appId: "1:239654343707:web:cce1fca89cd509ebc814be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
