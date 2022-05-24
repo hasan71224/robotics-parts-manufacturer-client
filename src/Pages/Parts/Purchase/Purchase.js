ed7e3658d228e335347c6337da600c3301a1ef5a
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-05-24 14:30:12 +0600

    purchase page workable

--- src/Pages/Parts/Purchase/Purchase.js
+++ src/Pages/Parts/Purchase/Purchase.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Purchase = () => {
     const { partsId } = useParams();
     const [parts, setParts] = useState({});
     const [purchaseQuantity, setPurchaseQuantity] = useState()
+    const [user, loading, error] = useAuthState(auth)
     useEffect(() => {
         const url = `http://localhost:5000/parts/${partsId}`;
         fetch(url)
@@ -12,13 +15,25 @@ const Purchase = () => {
             .then(data => setParts(data))
     }, [])
 
-    const handleQuantity=()=>{
+    let purchaseError;
+    if (error) {
+        purchaseError = <p className='text-red-500'>{error?.message}</p>
+    }
+
+    const handleQuantity = () => {
         let newQuantity = parseInt(purchaseQuantity)
-        newQuantity = parts.quantity - newQuantity
-        console.log(newQuantity);
-        let partsUpdate= {...parts, quantity: newQuantity}
-        console.log(partsUpdate);
-        setParts(partsUpdate)
+        if (newQuantity > 99 && newQuantity <= parts.quantity) {
+            newQuantity = parts.quantity - newQuantity
+            console.log(newQuantity);
+            let partsUpdate = { ...parts, quantity: newQuantity }
+            console.log(partsUpdate);
+            setParts(partsUpdate)
+        }
+        else {
+            // message: 'Must be 6 character or longer';
+            
+        }
+
     }
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl lg:mx-5 ">
@@ -34,7 +49,10 @@ const Purchase = () => {
                     <p> <b>Minimum Order Products:</b> {parts.minimumOrder}</p>
                     <p><b>{parts.quantity} {parts.quantity > 1 ? 'product' : 'product'} Available</b></p>
                     <p><b>Price:</b> $ {parts.price}</p>
-                    <p> <b>Purchase Quantity:</b> <input type="number" onChange={(e)=>setPurchaseQuantity(e.target.value)} placeholder="Purchase Quantity" onBlur={handleQuantity} class="input input-bordered input-sm w-full max-w-xs" /></p>
+                    <p> <b>Purchase Quantity:</b> <input type="number" onChange={(e) => setPurchaseQuantity(e.target.value)} placeholder="Purchase Quantity" onBlur={handleQuantity} class="input input-bordered input-sm w-full max-w-xs" /></p>
+                    {purchaseError}
+                    <input type="text" name='name' disabled value={user?.displayName || ''} className="input input-bordered w-full max-w-xs mt-2" />
+                    <input type="email" name='phone' disabled value={user?.email || ''} className="input input-bordered w-full max-w-xs mt-2" />
                 </div>
 
                 <div className="card-actions justify-center">
